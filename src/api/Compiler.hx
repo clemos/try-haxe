93f144be6b13b2e5b6a6db0f3a8ba08c7b8fc253
clemos <cl3mos@gmail.com>
2012-06-04 09:48:05 +0200

    better macro security

--- src/api/Compiler.hx
+++ src/api/Compiler.hx
@@ -38,7 +38,7 @@ class Compiler {
 		var mainFile = tmpDir + "/" + program.main.name + ".hx";
 
 		var source = program.main.source;
-		source = ~/@:macro/.customReplace( source , function( m ){ return ""; } );
+		source = ~/@([^:]*):([^a-z]*)macro/.customReplace( source , function( m ){ return ""; } );
 		
 		File.saveContent( mainFile , source );
 
