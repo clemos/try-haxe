4f6e0916da2a97cb52bf46d6c77bc183134dcae3
Clément Charmet <clement.charmet@ebuzzing.com>
2015-03-20 12:03:58 +0100

    manually merged PR #74

---
+++ examples/Example-22.hx
@@ -0,0 +1,31 @@
+class Test {
+    static function main() {
+        var r = new haxe.unit.TestRunner();
+        r.add(new MyTests());
+        r.run();
+    }
+}
+
+class MyTests extends haxe.unit.TestCase {
+    var myVal:String;
+    var myInt:Int;
+
+    override public function setup() {
+        myVal = "foo";
+        myInt = 1+1;
+    }
+    
+    /* Every test function name should start with 'test' */
+    
+    public function testValue() {
+        assertEquals("foo", myVal);
+    }
+    
+    public function testMath1() {
+        assertTrue(myInt == 2);
+    }
+    
+    public function testMath2() {
+        assertFalse(myInt == 3);
+    }
+}
