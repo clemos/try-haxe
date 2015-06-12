f86c48424a33566af3638ea892bf8afa945d95de
Gama11 <jensfischer95@gmail.com>
2015-06-12 12:10:00 +0200

    Add travis tests for the examples

--- examples/Example-20.hx
+++ examples/Example-20.hx
@@ -3,8 +3,8 @@ class Test {
 		draw();
 	}
 
-#if js
 	static function draw() {
+	#if js
 		var canvas = js.Browser.document.createCanvasElement();
 		canvas.width = 400;
 		canvas.height = 400;
@@ -24,9 +24,7 @@ class Test {
 			}
 			ctx.fill();
 		}
-	}
-#elseif flash
-	static function draw() {
+	#elseif flash
 		var ctx = flash.Lib.current.graphics;
 		for (part in graphic) {
 			ctx.beginFill(Std.parseInt('0x${part.color}'));
@@ -39,8 +37,8 @@ class Test {
 				}
 			}
 		}
+	#end
 	}
-#end
 
 	static var graphic = [{"color":"f68712","path":[{"x":45,"y":12},{"x":12,"y":45},{"x":45,"y":78},{"x":78,"y":45},{"x":45,"y":12}]},{"color":"fab20b","path":[{"x":2,"y":1},{"x":45,"y":12},{"x":12,"y":45},{"x":2,"y":1}]},{"color":"f89c0e","path":[{"x":2,"y":89},{"x":12,"y":45},{"x":45,"y":78},{"x":2,"y":89}]},{"color":"f47216","path":[{"x":89,"y":1},{"x":78,"y":45},{"x":45,"y":12},{"x":89,"y":1}]},{"color":"f25c19","path":[{"x":89,"y":89},{"x":45,"y":78},{"x":78,"y":45},{"x":89,"y":89}]},{"color":"fbc707","path":[{"x":45,"y":12},{"x":2,"y":1},{"x":23,"y":1},{"x":45,"y":12}]},{"color":"fbc707","path":[{"x":45,"y":12},{"x":89,"y":1},{"x":67,"y":1},{"x":45,"y":12}]},{"color":"f68712","path":[{"x":45,"y":78},{"x":89,"y":89},{"x":67,"y":89},{"x":45,"y":78}]},{"color":"f25c19","path":[{"x":45,"y":78},{"x":2,"y":89},{"x":23,"y":89},{"x":45,"y":78}]},{"color":"fff200","path":[{"x":12,"y":45},{"x":2,"y":89},{"x":2,"y":67},{"x":12,"y":45}]},{"color":"fff200","path":[{"x":12,"y":45},{"x":2,"y":1},{"x":2,"y":23},{"x":12,"y":45}]},{"color":"f1471d","path":[{"x":78,"y":45},{"x":89,"y":89},{"x":89,"y":67},{"x":78,"y":45}]},{"color":"f1471d","path":[{"x":78,"y":45},{"x":89,"y":1},{"x":89,"y":23},{"x":78,"y":45}]}];
 }
